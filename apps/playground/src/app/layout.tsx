f156e052bc95c6f66eb238532f12e6ca134a9bb7
taehyeon3657 <taehyeon3657@beaubrain.bio>
2025-11-20 15:18:01 +0900

    feat: vercel Web Analytics 태그 추가

--- apps/playground/src/app/layout.tsx
+++ apps/playground/src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={inter.className}>{children}</body>
     </html>
   );
